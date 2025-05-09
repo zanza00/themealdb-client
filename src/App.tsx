93b884da3828e47a46e6a24c848d4c228c25bdef
Simone Picciani <gh@simonepicciani.it>
2025-05-09 23:44:01 +0200

    install chakraUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import { useState } from 'react'
-import './App.css'
+import { Box, Button, Container, Heading, Flex } from '@chakra-ui/react'
+import { Provider } from './components/ui/provider'
+import { ColorModeProvider, ColorModeButton } from './components/ui/color-mode'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>The Meal DB Client</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Provider>
+      <ColorModeProvider>
+        <Container maxW="container.xl" centerContent>
+          <Flex justifyContent="space-between" alignItems="center" w="100%">
+            <Heading as="h1" size="xl" my={8}>
+              The Meal DB Client
+            </Heading>
+            <ColorModeButton />
+          </Flex>
+          <Box p={5} shadow="md" borderWidth="1px" borderRadius="md">
+            <Button colorScheme="blue" onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </Button>
+          </Box>
+        </Container>
+      </ColorModeProvider>
+    </Provider>
   )
 }
 
